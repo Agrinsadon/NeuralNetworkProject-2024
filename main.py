dc098ae65fc3025cddc192b01419c014d4eb2b86
agrin <agrin.sadon@metropolia.fi>
2024-10-21 12:45:54 +0200

    Added pytorch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import keras
 import tensorflow as tf
+import torch
 print( "keras version", keras.__version__)
 print("tensorflow version", tf.__version__)
+print("torch version", torch.__version__)
