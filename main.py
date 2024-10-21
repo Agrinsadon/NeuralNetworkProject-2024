0c17a07cc79823259d6923829d59517e1b4d6721
agrin <agrin.sadon@metropolia.fi>
2024-10-21 13:11:46 +0200

    Added a test to see if the packages work or not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import tensorflow
 import keras
-import tensorflow as tf
 import torch
-print( "keras version", keras.__version__)
-print("tensorflow version", tf.__version__)
-print("torch version", torch.__version__)
+
+print("Tensorflow version: ", tensorflow.__version__)
+print("Keras version: ", keras.__version__)
+print("PyTorch version: ", torch.__version__)
